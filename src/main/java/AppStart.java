1c377f358575df9fc04bd9bdeb86092e5ed7ce29
leejk118 <leejk118@gmail.com>
2019-04-10 12:51:36 +0900

    ADD inputMoney & generateLottoNumber

--- src/main/java/AppStart.java
+++ src/main/java/AppStart.java
@@ -1,4 +1,6 @@
-package PACKAGE_NAME;
-
 public class AppStart {
+    public static void main(String[] args) {
+        LottoGame lottoGame = new LottoGame();
+        lottoGame.start();
+    }
 }
--- src/main/java/LottoGame.java
+++ src/main/java/LottoGame.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import domain.Lotto;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class LottoGame {
+    private int lottoNum;
+    private List<Integer> numbers;
+    private Lotto[] userlotto;
+
+    public void start(){
+        inputMoney();
+        generateLottoNumber();
+        //inputWinningNumber();
+        //printWinningStatistic();
+    }
+
+    public void inputMoney(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("구입금액을 입력해 주세요.");
+        int money = sc.nextInt();
+        lottoNum = money / 1000;
+    }
+    public void generateLottoNumber(){
+        userlotto = new Lotto[lottoNum];
+        for (int i = 0; i < lottoNum; ++i){
+            generateRandomNumber();
+            userlotto[i] = new Lotto(numbers);
+        }
+    }
+    public void generateRandomNumber(){
+
+    }
 }
